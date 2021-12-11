494119653ec0d7933d954803ef768e71e42d2023
Rostyk Podolets <ros.podolets@gmail.com>
2021-12-12 00:57:49 +0200

    task-02 completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Profile from 'components/Profile/Profile';
+import Statistic from 'components/Statistic/Statistic';
 import user from './database/user.json';
+import data from './database/data.json';
 
 function App() {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistic title="Upload stats" stats={data} />
+      <Statistic stats={data} />
+    </>
   );
 }
 
