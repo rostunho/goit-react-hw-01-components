448082f78e48623f7d0581307da00f75822c7de0
Rostyk Podolets <ros.podolets@gmail.com>
2021-12-12 11:39:24 +0200

    task-03 completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@
 // import './App.css';
 import Profile from 'components/Profile/Profile';
 import Statistic from 'components/Statistic/Statistic';
+import FriendList from 'components/FriendList/FriendList';
 import user from './database/user.json';
 import data from './database/data.json';
+import friends from './database/friends.json';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       />
       <Statistic title="Upload stats" stats={data} />
       <Statistic stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 }
